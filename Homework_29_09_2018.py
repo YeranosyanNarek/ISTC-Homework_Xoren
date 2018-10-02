ebcd1733bcf00b9e210ad34aca002be0915d66e7
YeranosyanNarek <narekyeranosyan1111@gmail.com>
2018-10-02 14:18:36 +0400

    Homework_29_29_2018_1

---
+++ Homework_29_09_2018.py
@@ -0,0 +1,18 @@
+print('Enter the year')
+year = int(input())
+if 0 < year < 2006:
+    n1 = year//100
+    print('century From Year', year,  '=', n1 + 1)
+
+else:
+    print('Enter year between 1 and 2005')
+
+
+
+
+
+
+
+
+
+
---
+++ SRT_COUNT_SIMB.py
@@ -0,0 +1,13 @@
+
+import sys
+
+string_fix = 'abcdefghijklmnopqrstuvwxyz'
+string = sys.argv[1]
+
+
+
+
+for i, in string_fix:
+    print(i,':', string.count(i))
+    
+
