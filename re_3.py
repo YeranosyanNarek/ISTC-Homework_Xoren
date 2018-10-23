268a1dc5f6cc8ec99ff89f9cb3643b1020b7c485
YeranosyanNarek <narekyeranosyan1111@gmail.com>
2018-10-24 00:35:54 +0400

    re_3

---
+++ re_3.py
@@ -0,0 +1,14 @@
+import re
+str1 = 'Being able to match varying sets of characters is the first thing regular' \
+       ' expressions can do that isn’t already possible with the methods available ' \
+       'on strings. However, if that was the only additional capability of regexes, ' \
+       'they wouldn’t be much of an advance. Another capability is that you can specify ' \
+       'that portions of the RE must be repeated a certain number of times.'
+str2 = 'abbbbbcbbbbdfghijkabaaaaa'
+
+p = re.compile('ab+')
+print(re.findall(p, str2))
+print('===========')
+print(re.findall(p, str1))
+
+
