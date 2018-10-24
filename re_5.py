0dc67d84c900ad187ab6bb3ec6181025e8b80356
YeranosyanNarek <narekyeranosyan1111@gmail.com>
2018-10-24 13:11:24 +0400

    re_5_RevA

--- re_5.py
+++ re_5.py
@@ -6,7 +6,7 @@ str1 = 'Being able to match varying sets of characters is the first thing regula
        'that portions of the RE must be repeated a certain number of times.'
 str2 = 'abbbbbcbbbbdfghijkabaaaaa'
 
-p = re.compile('abbb')
+p = re.compile('ab{3}')
 print(re.findall(p, str2))
 print('===========')
 print(re.findall(p, str1))
